1c4c49c2f5b8dfb308445641e0412057163304de
krupp <44751363+zzfn@users.noreply.github.com>
2024-03-27 22:43:43 +0800

    Update createSignal.ts

--- createSignal.ts
+++ createSignal.ts
@@ -5,7 +5,7 @@ type Options = { equals?: boolean };
 type signal<T> = [get: Accessor<T>, set: Setter<T>];
 type Accessor<T> = () => T;
 
-export function createSignal<T>(initialValue: T, options?: Options): signal<T> {
+export function createSignal<T>(initialValue?: T, options?: Options): signal<T> {
     const r = shallowRef(initialValue)
     const getter: Accessor<T> = () => r.value
     const setter: Setter<T> = (v: unknown) => {
