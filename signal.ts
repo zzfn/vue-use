ffd835b2cfa6df12da1af41daad4854b23910034
krupp <44751363+zzfn@users.noreply.github.com>
2024-03-27 22:16:26 +0800

    Update signal.ts

--- signal.ts
+++ signal.ts
@@ -1,11 +1,18 @@
-import { shallowRef } from 'vue'
+import {shallowRef, triggerRef} from 'vue'
 
-export function signal(initialValue) {
-    const r = shallowRef(value)
-    const get = () => r.value
-    const set = (v) => {
-        r.value = typeof v === 'function' ? v(r.value) : v
+type Setter<T> = (v: T | ((prev?: T) => T)) => T;
+type Options = { equals?: boolean };
+type signal<T> = [get: Accessor<T>, set: Setter<T>];
+type Accessor<T> = () => T;
+
+export function createSignal<T>(initialValue: T, options?: Options): signal<T> {
+    const r = shallowRef(initialValue)
+    const get: Accessor<T> = () => r.value
+    const set: Setter<T> = (v) => {
+        r.value = typeof v === 'function' ? (v as (prev?: T) => T)(r.value) : v
+        // r.value = typeof v === 'function' ? v(r.value) : v
         if (options?.equals === false) triggerRef(r)
+        return r.value;
     }
     return [get, set]
 }
