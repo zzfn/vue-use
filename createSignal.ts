1e4691f2ba75ce5028dc55ff85e2bf2dd24caf68
krupp <44751363+zzfn@users.noreply.github.com>
2024-03-27 23:02:42 +0800

    Update createSignal.ts

--- createSignal.ts
+++ createSignal.ts
@@ -1,6 +1,7 @@
 import {shallowRef, triggerRef} from 'vue'
+import {produce} from "immer"
 
-type Setter<T> = (v: T | ((prev: T) => T)) => T;
+type Setter<T> = (v: T | ((prev: T) => T | void)) => T;
 type Options = { equals?: boolean };
 type signal<T> = [get: Accessor<T>, set: Setter<T>];
 type Accessor<T> = () => T;
@@ -9,7 +10,7 @@ export function createSignal<T>(initialValue?: T, options?: Options): signal<T> 
     const r = shallowRef(initialValue)
     const getter: Accessor<T> = () => r.value
     const setter: Setter<T> = (v: unknown) => {
-        r.value = typeof v === 'function' ? v(r.value) : v
+        r.value = typeof v === 'function' ? produce<T>(r.value, v) : v
         if (options?.equals === false) triggerRef(r)
         return r.value;
     }
